0c7ccf0c41f0610954170617090570880084a040
Maxim Meleshkin <maximmms@hotmail.com>
2025-04-07 21:33:50 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
 ## Ваш код
 url = 'https://habr.com/ru/articles/'
-response = requests.get(url, headers=Headers(os='win', browser='chrome').generate())
+try:
+    response = requests.get(url, headers=Headers(os='win', browser='chrome').generate())
+except Exception as e:
+    print('Ресурс недоступен, проверьте наличие подключения к сети')
 
 soup = bs4.BeautifulSoup(response.text, 'lxml')
 #получаем список статей
 articles = soup.find_all('article')
 for article in articles:
-    article_link = url + article.attrs.get('id')
+    relative_link = article.find('a', class_='tm-title__link')['href']
+    article_link = 'https://habr.com' + relative_link
+    # article_link = url + article.attrs.get('id')
     article_date = article.find('time')['datetime']
     article_header = article.find('h2').text
     #скачиваем страницу со статьёй
     response = requests.get(article_link, headers=Headers(os='win', browser='chrome').generate())
     soup = bs4.BeautifulSoup(response.text, 'lxml')
     #получаем текст статьи
-    article_text = soup.find('div', attrs={'xmlns': 'http://www.w3.org/1999/xhtml'})
+    article_body = soup.find('div', class_='tm-article-body')
+    article_text = article_body.get_text().lower() if article_body else ''
     #проверяем наличие в тексте искомых слов и выводим в консоль если найдены
-    for paragraph in article_text:
-        if any(word in paragraph.text for word in KEYWORDS):
-            #<дата> – <заголовок> – <ссылка>
-            print(f'{article_date} – {article_header} – {article_link}')
-            break
+
+    if any(word.lower() in article_text for word in KEYWORDS):
+        #<дата> – <заголовок> – <ссылка>
+        print(f'{article_date} – {article_header} – {article_link}')
